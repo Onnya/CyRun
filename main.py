03d420e635414f6e9d6306e77879973e7e6d8721
Onnya <timur.kvashin.a@gmail.com>
2021-02-22 21:02:37 +0300

    Border generator added

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ pygame.mouse.set_visible(0)
 all_sprites = pygame.sprite.Group()
 players = pygame.sprite.Group()
 objects = pygame.sprite.Group()
+borders = pygame.sprite.Group()
 clock = pygame.time.Clock()
 
 
 class Camera:
     def __init__(self):
         self.dx = 0
-        self.dy = 0
 
     def apply(self, obj):
         obj.rect.x += self.dx
@@ -34,12 +34,19 @@ class Camera:
 
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, length):
         super().__init__(objects, all_sprites)
-        self.image = pygame.Surface([700, 10])
+        self.image = pygame.Surface([length, 10])
         self.image.fill("black")
-        self.rect = pygame.Rect(0, 440, 700, 10)
-        pygame.draw.rect(self.image, pygame.Color("black"), (0, 440, 700, 10))
+        self.rect = pygame.Rect(0, 440, length, 10)
+
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x, h):
+        super().__init__(borders, all_sprites)
+        self.image = pygame.Surface([1, h])
+        self.image.set_colorkey(self.image.get_at((0, 0)))
+        self.rect = pygame.Rect(x, 0, 1, h)
 
 
 class Player(pygame.sprite.Sprite):
@@ -51,7 +58,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
         self.rect.x = pos[0]
-        self.rect.y = pos[1] - self.rect.h
+        self.rect.y = pos[1] - self.rect.h - 10
 
     def update(self, *args):
         global speed
@@ -74,6 +81,9 @@ class Player(pygame.sprite.Sprite):
                 if speed != 0:
                     speed += 1
         self.rect.x += speed
+        if pygame.sprite.spritecollideany(self, borders):
+            self.rect.x -= speed
+            speed = 0
 
 
 bg = parallax.ParallaxSurface((700, 450), pygame.RLEACCEL)
@@ -81,7 +91,8 @@ bg.add('far-buildings.png', 17)
 bg.add('back-buildings.png', 9)
 bg.add('foreground.png', 5)
 
-floor = Floor()
+floor = Floor(1500)
+lb = Border(0, 500)
 pl = Player(floor.rect.bottomleft)
 camera = Camera()
 
